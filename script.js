7a368ddfd65425fa5410107f73402731b72fc1e0
braian123007 <136755802+braian123007@users.noreply.github.com>
2024-11-25 12:28:03 -0300

    ..

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function verificar {
+function verificar() {
     var n1 = document.getElementById('nota1').value;
     var n2 = document.getElementById('nota2').value;
     var n3 = document.getElementById('nota3').value;
