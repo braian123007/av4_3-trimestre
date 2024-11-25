587517b46637a0cf3f37153e70a60ee4e86045a0
braian123007 <136755802+braian123007@users.noreply.github.com>
2024-11-25 12:18:02 -0300

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+function verificar {
+    var n1 = document.getElementById('nota1').value;
+    var n2 = document.getElementById('nota2').value;
+    var n3 = document.getElementById('nota3').value;
+}
