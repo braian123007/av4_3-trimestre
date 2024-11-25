446e68cafd42de2b0447579ca1fba2bbdd4e707c
braian123007 <136755802+braian123007@users.noreply.github.com>
2024-11-25 12:25:00 -0300

    .

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ function verificar {
     var n1 = document.getElementById('nota1').value;
     var n2 = document.getElementById('nota2').value;
     var n3 = document.getElementById('nota3').value;
+    var media = (parseFloat(n1)+parseFloat(n2)+parseFloat(n3))/3;
+
+    if (media >=60){
+        resultado.textContent= 'aprovado';
+    } else {
+        resultado.textcontent= 'reprovado';
+    }
 }
